282327c3083184c6d41f2cc7e44a5b87964a2424
lw <root@fsoc.space>
2019-11-20 14:08:37 +0100

    Add proper input type value

--- infp/php/login/login.php
+++ infp/php/login/login.php
@@ -20,7 +20,7 @@ else $content='home.php';
     <main>
         <form action="login_check.php" method="post" enctype="multipart/form-data">
             <label>Username: <input type="text" name="username"></label><br>
-            <label>Password: <input type="text" name="password"></label><br>
+            <label>Password: <input type="password" name="password"></label><br>
             <button type="submit" name="send">Submit</button>
         </form>
     </main>
